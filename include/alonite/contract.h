5dc888925d91f18704185ade454ec95b93e07648
Nicolai Trandafil <nicktrandafil@gmail.com>
2024-08-04 22:34:43 +0300

    `alonite_assert` accepts optional message
    Documented `WhenAny`.
    Added annotation where synchronization is required.

--- include/alonite/contract.h
+++ include/alonite/contract.h
@@ -27,28 +27,47 @@ struct Todo {
     }
 };
 
-#define alonite_todo()                                                                       \
-    alonite::Todo {                                                                          \
+#define alonite_todo()                                                                   \
+    alonite::Todo {                                                                      \
         std::source_location::current()                                                  \
     }
 
 struct Invariant {
-    [[noreturn]] void failed(std::source_location const& l) {
+    template <class... Ts> // todo: optional<string_view>
+    [[noreturn]] void failed(std::source_location const& l, Ts... msg) {
 #ifdef alonite_ABORT_ON_INVARIANT_VIOLATION
         static_cast<void>(l);
         std::abort();
 #else
-        throw std::runtime_error(std::format("invariant failed at {}:{}:{} ({})",
-                                             l.file_name(),
-                                             l.line(),
-                                             l.column(),
-                                             l.function_name()));
+        if constexpr (sizeof...(msg) > 0) {
+            throw std::runtime_error(std::format("invariant failed at {}:{}:{} ({}) - {}",
+                                                 l.file_name(),
+                                                 l.line(),
+                                                 l.column(),
+                                                 l.function_name(),
+                                                 msg...));
+        } else {
+            throw std::runtime_error(std::format("invariant failed at {}:{}:{} ({})",
+                                                 l.file_name(),
+                                                 l.line(),
+                                                 l.column(),
+                                                 l.function_name()));
+        }
 #endif
     }
 };
 
-#define alonite_assert(expr, module)                                                         \
+#define alonite_assert_2_args(expr, module)                                              \
     ((expr) || (module.failed(std::source_location::current()), true), AnyExpr{})
+#define alonite_assert_3_args(expr, module, msg)                                         \
+    ((expr) || (module.failed(std::source_location::current(), msg), true), AnyExpr{})
+
+#define get_4rd_arg(arg1, arg2, arg3, arg4, ...) arg4
+#define alonite_assert_argument_chooser(...)                                             \
+    get_4rd_arg(__VA_ARGS__, alonite_assert_3_args, alonite_assert_2_args, )
+
+/// alonite_assert(expr, module, opitonal<string_view>)
+#define alonite_assert(...) alonite_assert_argument_chooser(__VA_ARGS__)(__VA_ARGS__)
 
 struct AnyExpr {
     [[maybe_unused]] AnyExpr() noexcept = default;
@@ -59,7 +78,7 @@ struct AnyExpr {
     }
 };
 
-#define alonite_unreachable(module)                                                          \
+#define alonite_unreachable(module)                                                      \
     (module.failed(std::source_location::current()), AnyExpr{})
 
 } // namespace alonite
