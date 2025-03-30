d9fc154dc8a47fb6b241612e26c7c89517410d81
Nicolai Trandafil <nicktrandafil@gmail.com>
2025-03-30 22:44:17 +0300

    Connect Qt signal to channel

--- include/alonite/qt.h
+++ include/alonite/qt.h
@@ -31,110 +31,22 @@ struct Disconnected : Error {
     }
 };
 
-/// Signal awaiter.
-/// It is a lightweight copiable object, that connects the signal on constructor
-/// and disconnects when the last copy of it is destructed. When the sender object is
-/// destructed or disconnected while something is awaiting this, the upstream will be continued with
-/// the exception `Disconnected`.
-template <class T, class... Args>
-class [[nodiscard]] Signal {
-public:
-    Signal(T& obj, void (T::*sig)(Args...)) {
-        std::shared_ptr<Receiver> state{new Receiver{}};
-        state->set_connection(QObject::connect(&obj, sig, [state](Args const&... args) {
-            (*state)(args...);
-        }));
-        this->state = state;
+// Connect a signal to a channel
+template <class T, class Sink, class... Args>
+    requires (sizeof...(Args) > 1 && requires (Sink s, Args const&... args) { { s.send(std::tuple{args...}) }; })
+          || (sizeof...(Args) <= 1 && requires(Sink s, Args const&... args) { { s.send(args...) }; })
+auto connect(T& obj, void (T::*sig)(Args...), Sink channel) {
+    if constexpr (sizeof...(Args) > 1) {
+        return QObject::connect(
+                &obj, sig, [channel = std::move(channel)](Args const&... args) {
+                    channel.send(std::tuple(args...));
+                });
+    } else {
+        return QObject::connect(
+                &obj, sig, [channel = std::move(channel)](Args const&... args) {
+                    channel.send(args...);
+                });
     }
-
-    Signal(Signal const&) = default;
-    Signal& operator=(Signal const&) = default;
-
-    Signal(Signal&&) = default;
-    Signal& operator=(Signal&&) = default;
-
-    bool await_ready() const noexcept {
-        return false;
-    }
-
-    template <class U>
-    void await_suspend(std::coroutine_handle<U> caller) {
-        if (auto const state = this->state.lock()) {
-            auto stack = caller.promise().stack.lock();
-            alonite_assert(stack, Invariant{});
-            stack->erased_top().ex->increment_external_work();
-            this->caller = caller.promise().stack;
-            state->add_receiver(std::weak_ptr{caller.promise().stack});
-        } else {
-            throw Disconnected{};
-        }
-    }
-
-    std::tuple<Args...> await_resume() {
-        auto const stack = caller.value().lock();
-        alonite_assert(stack, Invariant{});
-        return stack->take_result<std::tuple<Args...>>();
-    }
-
-private:
-    class Receiver {
-    public:
-        ~Receiver() {
-            if (auto connection = take(this->connection)) {
-                QObject::disconnect(*connection);
-            }
-
-            std::vector<std::weak_ptr<TaskStack>> coros;
-
-            {
-                std::scoped_lock lock{mutex};
-                std::swap(coros, this->coros);
-            }
-
-            for (std::weak_ptr<TaskStack> const& coro : coros) {
-                if (auto const x = coro.lock()) {
-                    x->put_result(std::make_exception_ptr(Disconnected{}));
-                    auto const ex = x->erased_top().ex;
-                    schedule(std::shared_ptr{x});
-                    ex->decrement_external_work();
-                }
-            }
-        }
-
-        void operator()(Args const&... args) {
-            std::vector<std::weak_ptr<TaskStack>> coros;
-
-            {
-                std::scoped_lock lock{mutex};
-                std::swap(coros, this->coros);
-            }
-
-            for (std::weak_ptr<TaskStack> const& coro : coros) {
-                if (auto x = coro.lock()) {
-                    x->put_result(std::tuple(args...));
-                    auto const ex = x->erased_top().ex;
-                    schedule(std::shared_ptr{x});
-                    ex->decrement_external_work();
-                }
-            }
-        }
-
-        void set_connection(QMetaObject::Connection&& connection) {
-            this->connection = std::move(connection);
-        }
-
-        void add_receiver(std::weak_ptr<TaskStack>&& receiver) {
-            std::scoped_lock lock{mutex};
-            coros.push_back(std::move(receiver));
-        }
-
-        std::optional<QMetaObject::Connection> connection;
-        std::vector<std::weak_ptr<TaskStack>> coros;
-        std::mutex mutex;
-    };
-
-    std::optional<std::weak_ptr<TaskStack>> caller;
-    std::weak_ptr<Receiver> state;
-};
+}
 
 } // namespace alonite
--- test/qt.cpp
+++ test/qt.cpp
@@ -1,3 +1,4 @@
+#include <alonite/mpsc.h>
 #include <alonite/qt.h>
 
 #include <catch2/catch_all.hpp>
@@ -21,11 +22,21 @@ public:
         emit my_signal(x);
     }
 
+    void emit_my_signal0() {
+        emit my_signal0();
+    }
+
+    void emit_my_signal2(int x, std::string const& y) {
+        emit my_signal2(x, y);
+    }
+
 signals:
     void my_signal(int);
+    void my_signal0();
+    void my_signal2(int, std::string const&);
 };
 
-TEST_CASE("wait a signal", "[Signal][qt]") {
+TEST_CASE("wait a signal", "[connect][qt]") {
     using namespace std::chrono_literals;
 
     int c = 0;
@@ -33,14 +44,80 @@ TEST_CASE("wait a signal", "[Signal][qt]") {
     QEventLoop qexec;
     MyObject my_object;
     QTimer timer;
-    QTimer::singleShot(10ms, &my_object, [&my_object] { my_object.emit_my_signal(10); });
-    QTimer::singleShot(15ms, &qexec, [&qexec] { qexec.quit(); });
+    QTimer::singleShot(10ms, &my_object, [&my_object] {
+        my_object.emit_my_signal(10);
+    });
+    QTimer::singleShot(15ms, &qexec, [&qexec] {
+        qexec.quit();
+    });
 
     ThisThreadExecutor exec;
     std::thread t{[&my_object, &exec] {
         exec.block_on([](auto& my_object) -> Task<void> {
-            auto res = co_await Signal{my_object, &MyObject::my_signal};
-            REQUIRE(res == std::tuple(10));
+            auto [tx, rx] = mpsc::unbound_channel<int>();
+            connect(my_object, &MyObject::my_signal, tx);
+            auto res = co_await rx.recv();
+            REQUIRE(res == 10);
+            co_return;
+        }(my_object));
+    }};
+
+    qexec.exec();
+    t.join();
+}
+
+TEST_CASE("wait a void signal", "[connect][qt]") {
+    using namespace std::chrono_literals;
+
+    int c = 0;
+    QCoreApplication app(c, nullptr);
+    QEventLoop qexec;
+    MyObject my_object;
+    QTimer timer;
+    QTimer::singleShot(10ms, &my_object, [&my_object] {
+        my_object.emit_my_signal0();
+    });
+    QTimer::singleShot(15ms, &qexec, [&qexec] {
+        qexec.quit();
+    });
+
+    ThisThreadExecutor exec;
+    std::thread t{[&my_object, &exec] {
+        exec.block_on([](auto& my_object) -> Task<void> {
+            auto [tx, rx] = mpsc::unbound_channel<void>();
+            connect(my_object, &MyObject::my_signal0, tx);
+            auto res = co_await rx.recv();
+            REQUIRE(res == true);
+            co_return;
+        }(my_object));
+    }};
+
+    qexec.exec();
+    t.join();
+}
+
+TEST_CASE("wait a multi arguments signal", "[connect][qt]") {
+    using namespace std::chrono_literals;
+
+    int c = 0;
+    QCoreApplication app(c, nullptr);
+    QEventLoop qexec;
+    MyObject my_object;
+    QTimer timer;
+    QTimer::singleShot(10ms, &my_object, [&my_object] {
+        my_object.emit_my_signal2(1, "hello world");
+    });
+    QTimer::singleShot(15ms, &qexec, [&qexec] {
+        qexec.quit();
+    });
+
+    ThisThreadExecutor exec;
+    std::thread t{[&my_object, &exec] {
+        exec.block_on([](auto& my_object) -> Task<void> {
+            auto [tx, rx] = mpsc::unbound_channel<std::tuple<int, std::string>>();
+            connect(my_object, &MyObject::my_signal2, tx);
+            auto const res = co_await rx.recv();
+            REQUIRE(res == std::tuple{1, std::string{"hello world"}});
             co_return;
         }(my_object));
     }};
@@ -59,18 +136,19 @@ TEST_CASE("wait a signal twice", "[Signal][qt]") {
     QTimer::singleShot(10ms, &my_object, [&my_object] {
         my_object.emit_my_signal(10);
     });
-    QTimer::singleShot(100ms, &my_object, [&my_object] {
+    QTimer::singleShot(11ms, &my_object, [&my_object] {
         my_object.emit_my_signal(11);
     });
 
     ThisThreadExecutor exec;
     std::thread t{[&my_object, &exec, &app] {
         exec.block_on([](auto& my_object, auto& app) -> Task<void> {
-            Signal signal{my_object, &MyObject::my_signal};
-            auto const r1 = co_await signal;
-            REQUIRE(r1 == std::tuple(10));
-            auto const r2 = co_await signal;
-            REQUIRE(r2 == std::tuple(11));
+            auto [tx, rx] = mpsc::unbound_channel<int>();
+            connect(my_object, &MyObject::my_signal, std::move(tx));
+            auto const r1 = co_await rx.recv();
+            REQUIRE(r1 == 10);
+            auto const r2 = co_await rx.recv();
+            REQUIRE(r2 == 11);
             app.quit();
             co_return;
         }(my_object, app));
@@ -86,6 +164,7 @@ TEST_CASE("How to get into coroutine context", "[QExecutor][qt]") {
     QCoreApplication app(c, nullptr);
     QExecutor exec;
     spawn([](auto& app) -> Task<void> {
+        co_await Yield{};
         app.quit();
         co_return;
     }(app));
